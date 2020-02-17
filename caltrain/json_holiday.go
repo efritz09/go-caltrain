f2e9f7a68e77eeb00ea1e5a86977f4bf8745a271
eric <efritz09@gmail.com>
2020-02-16 21:53:53 -0800

    Add emergency holiday parsing to handle bad json data add makefile

--- caltrain/json_holiday.go
+++ caltrain/json_holiday.go
@@ -15,3 +15,19 @@ type holidayJson struct {
 		} `json:"AvailabilityConditions"`
 	} `json:"Content"`
 }
+
+type holidayJsonAlt struct {
+	Content struct {
+		ServiceCalendar struct {
+			ID       string `json:"id"`
+			FromDate string `json:"FromDate"`
+			ToDate   string `json:"ToDate"`
+		} `json:"ServiceCalendar"`
+		AvailabilityConditions struct {
+			Version  string `json:"version"`
+			ID       string `json:"id"`
+			FromDate string `json:"FromDate"`
+			ToDate   string `json:"ToDate"`
+		} `json:"AvailabilityConditions"`
+	} `json:"Content"`
+}
